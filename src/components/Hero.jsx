7cb27febc5260f6c35aed5a4da3d45575dac6a7d
saadchow <109829705+saadchow@users.noreply.github.com>
2023-09-30 17:29:21 -0400

    completed project

---
+++ src/components/Hero.jsx
@@ -0,0 +1,23 @@
+import { logo } from '../assets';
+
+const Hero = () => {
+  return (
+    <header className='w-full flex justify-center items-center flex-col'>
+        <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+            <img src={logo} alt="sum_logo" className="w-28 object-contain" />
+            <button type="button" onClick={()=> window.open ('https://github.com')} className='black_btn'>
+                GitHub
+            </button>
+        </nav>
+        <h1 className='head_text'>
+            Summarize any article using the capabilities of <br className='max:md:hidden' />
+            <span className="blue_gradient">OpenAI's GPT-4</span>
+        </h1>
+        <h2 className='desc'>
+            Experience a streamlined reading process with Summ.ary, an open-source tool that condenses lengthy articles into clean summaries.
+        </h2>
+    </header>
+  )
+}
+
+export default Hero
